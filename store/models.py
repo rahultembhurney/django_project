76b09fb291756264a287ac19603ce73b3055a8b2
rahultembhurney <rahul.tembhurney@here.com>
2022-02-28 23:47:22 +0530

    changes made to IntegerValue in Phone number in store class to CharField

--- store/models.py
+++ store/models.py
@@ -49,7 +49,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    phone = models.SmallIntegerField()
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
 
     membership_bronze = 'B'
